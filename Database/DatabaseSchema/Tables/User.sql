6d63d3b72decca3173550d44a74abcb999d6ec6d
Dmitry Elishevich <delishevich@tula.co>
2018-09-20 22:06:14 +0300

    Create database project and add User table to it.

---
+++ Database/DatabaseSchema/Tables/User.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id]            INT             NOT NULL        PRIMARY KEY,
+	[Name]          NVarchar(50)    NOT NULL,
+	[Surname]       NVARCHAR(50)    NOT NULL,
+	[Patronymic]    NVARCHAR(50)    NULL,
+	[Birthday]      Date            NULL,
+	[Email]         NVarchar(50)    NULL,
+	[Phone]         NVarchar(20)    NUll
+);
+GO
