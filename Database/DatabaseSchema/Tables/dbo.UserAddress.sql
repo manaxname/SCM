7d0fffe92106ce5b6c26c07875f00c8a9e160840
Dmitry Elishevich <delishevich@tula.co>
2018-09-20 23:12:58 +0300

    Add UserAddress table to schema.

---
+++ Database/DatabaseSchema/Tables/dbo.UserAddress.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[UserAddress]
+(
+	[Id]              INT       NOT NULL        PRIMARY KEY,
+	[UserId]          INT       NOT NULL,
+	[AddressId]       INT       NOT NULL,
+	CONSTRAINT [FK_UserAddress_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([Id]),
+	CONSTRAINT [FK_UserAddress_Address] FOREIGN KEY ([AddressId]) REFERENCES [dbo].[Address] ([Id]),
+);
+GO
