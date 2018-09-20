10fc034f410b9e9df682586adca4b22cf5904750
Dmitry Elishevich <delishevich@tula.co>
2018-09-20 22:19:13 +0300

    Add Address table to schema.

---
+++ Database/DatabaseSchema/Tables/dbo.Address.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Address]
+(
+	[Id]                  INT                   NOT NULL         PRIMARY KEY,
+	[NumberOfBuildings]   SMALLINT              NOT NULL,
+	[Zip]                 INT                   NOT NULL,
+	[Street]              NVARCHAR(160)         NOT NULL,
+	[City]                NVARCHAR(50)          NOT NULL,
+	[District]            NVARCHAR(50)          NULL,
+	[State]               NVARCHAR(50)          NULL,
+	[Country]             NVARCHAR(30)          NOT NULL
+);
+GO
+
+
