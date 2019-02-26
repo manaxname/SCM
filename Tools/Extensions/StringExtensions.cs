4871ef1b37ef4b570b238e4bcf1d80a4d06d348c
Dmitry Elishevich <delishevich@tula.co>
2019-02-26 21:17:26 +0300

    Implement StringExtensions.

---
+++ Tools/Extensions/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Tools.Extensions
+{
+	public static class StringExtensions
+	{
+		public static bool IsEmpty(this string origin)
+		{
+			return string.IsNullOrEmpty(origin);
+		}
+
+		public static string Union(this IEnumerable<string> array, string separator)
+		{
+			return string.Join(separator, array);
+		}
+
+		public static string Format(this string origin, string substitute, params string[] substitutes)
+		{
+			return string.Format(origin, substitute, substitutes);
+		}
+	}
+}
